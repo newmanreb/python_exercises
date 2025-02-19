7a874912c6b36eb5551776d7987387d04ad8c300
Rebekah <rebekah.s.newman@gmail.com>
2025-02-19 12:12:42 +0000

    feb 17 changes

--- python_exercise_1.py
+++ python_exercise_1.py
@@ -60,3 +60,18 @@ for i in range(0, len(sequence), block_size):
 
 ## 1.5
 # Write code to transcribe a DNA sequence to RNA.
+# Given the following sequence:
+example_seq = "aggagtaagcccttgcaactggaaatacacccattg"
+# The output should be "aggaguaagcccuugcaacuggaaauacacccauug"
+def transcribe(dna_seq):
+    """Transcribes DNA to RNA by replacing T with U."""
+    return dna_seq.upper().replace("T","U")
+
+rna_seq_example = transcribe(example_seq)
+print("RNA Example Sequence:", rna_seq_example.lower())
+
+# Now given the following sequence, transcribe to RNA:
+DNA = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTGAT"
+
+RNA = transcribe(DNA)
+print("RNA Sequence:", RNA)
